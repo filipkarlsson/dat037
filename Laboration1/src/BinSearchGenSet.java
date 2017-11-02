e28efac0fa5113f50bf66a1bdbdb6056eb9b450b
Filip Karlsson <filip@filipkarlsson.com>
2017-11-02 10:09:12 +0100

    GenSet interface and BinSearchGenSet class created

--- Laboration1/src/BinSearchGenSet.java
+++ Laboration1/src/BinSearchGenSet.java
@@ -1,5 +1,23 @@
 /**
  * Created by filip on 2017-11-02.
  */
-public class BinSearchGenSet {
+public class BinSearchGenSet<E extends Comparable<? super E>> implements GenSet<E>{
+
+
+
+    @Override
+    public void remove(E element) {
+
+    }
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
 }
