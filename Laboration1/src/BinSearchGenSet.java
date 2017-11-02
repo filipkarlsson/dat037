5cb8f82e90f66195992ff799721516a64e969ad1
Filip Karlsson <filip@filipkarlsson.com>
2017-11-02 09:59:10 +0100

    GenSet interface and BinSearchGenSet class created

---
+++ Laboration1/src/BinSearchGenSet.java
@@ -0,0 +1,5 @@
+/**
+ * Created by filip on 2017-11-02.
+ */
+public class BinSearchGenSet {
+}
---
+++ Laboration1/src/GenSet.java
@@ -0,0 +1,8 @@
+/**
+ * Created by filip on 2017-11-02.
+ */
+public interface GenSet<E> {
+    public void add(E element);
+    public boolean contains(E element);
+    public void remove(E element);
+}
