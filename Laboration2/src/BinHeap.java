548730d8e8a1cfe344e722718a647fb6c94c21b7
Filip Karlsson <filip@filipkarlsson.com>
2017-11-15 08:06:20 +0100

    TestBinHeap now works fine!

--- Laboration2/src/BinHeap.java
+++ Laboration2/src/BinHeap.java
@@ -1,3 +1,6 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
+
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -24,22 +27,36 @@ public class BinHeap<E> implements PrioQueue<E>{
 
     @Override
     public E peek() {
-        return heap.get(0);
+        if (heap.size() > 0) {
+            return heap.get(0);
+        }
+        else {
+            return null;
+        }
     }
 
     @Override
     public E poll() {
-        E root = heap.get(0);
-        heap.remove(root);
-        return root;
+        if (heap.size() > 0){
+            E root = heap.get(0);
+            remove(root);
+            return root;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void remove(E e) {
         int i = heap.indexOf(e);
-        swap(i, heap.size() - 1);
-        heap.remove(heap.size() - 1);
-        bubbleDown(i);
+        if ( (i >= 0) && (heap.size() > 0) ){
+            swap(i, heap.size() - 1);
+            heap.remove(heap.size() - 1);
+            if (heap.size() != i) {
+                bubbleDown(i);
+                bubbleUp(i);
+            }
+        }
     }
 
     @Override
@@ -52,7 +69,7 @@ public class BinHeap<E> implements PrioQueue<E>{
     private void bubbleUp(int i){
         if (comp.compare(heap.get(i), heap.get((i-1)/2)) < 0){
             // byt plats
-            swap(i,(i-2)/2);
+            swap(i,(i-1)/2);
             bubbleUp((i-1)/2);
         }
         else {
@@ -61,14 +78,34 @@ public class BinHeap<E> implements PrioQueue<E>{
     }
 
     private void bubbleDown(int i){
-        if (comp.compare(heap.get(i), heap.get(2*i + 1)) > 0){
-            // byt plats
-            swap(i, 2*i+1);
-            bubbleDown(2*i+1);
-        }
-        else {
+        E leftChild;
+        E rightChild;
+        if (heap.size() >= 2*i + 3){ // There exists 2 children to the current node
+            leftChild = heap.get(2*i + 1);
+            rightChild = heap.get(2*i + 2);
+            if (comp.compare(rightChild, leftChild) < 0){ // The smallest is two the right
+                if (comp.compare(heap.get(i), rightChild) > 0){ // The node is greater than it's right child
+                    swap(i, 2*i + 2);
+                    bubbleDown(2*i + 2); // Bubble to the right
+                }
+            } else{
+                if (comp.compare(heap.get(i), leftChild) > 0){ // The node is greater than it's left child
+                    swap(i, 2*i + 1);
+                    bubbleDown(2*i + 1); // Bubble to the left
+                }
+            }
+
+        } else if (heap.size() >= 2*i + 2) { // There exists one child.
+            leftChild = heap.get(2*i + 1);
+            if (comp.compare(heap.get(i), leftChild) > 0){ // The node is greater than it's left child
+                swap(i, 2*i + 1);
+                bubbleDown(2*i + 1); // Bubble to the left
+            }
+        } else {
             return;
         }
+
+
     }
 
     private void swap(int i, int j){
@@ -76,4 +113,14 @@ public class BinHeap<E> implements PrioQueue<E>{
         heap.set(i, heap.get(j));
         heap.set(j,tmp);
     }
+
+    public String toString(){
+        String s = "";
+        for (E e : heap){
+            s += e.toString();
+            s += " ";
+        }
+        return s;
+    }
+
 }
--- Laboration2/src/ManTest.java
+++ Laboration2/src/ManTest.java
@@ -6,18 +6,27 @@ import java.util.Comparator;
 public class ManTest {
     public static void main(String[] args) {
         Comparator comp = new NaturalOrderComparator();
-        BinHeap<Integer> binHeap = new BinHeap(comp);
+        BinHeap<Integer> pq = new BinHeap(comp);
+
+        pq.peek();  // result: null
+        System.out.println(pq.toString());
+        pq.add(2);
+        System.out.println(pq.toString());
+        pq.remove(6);
+        System.out.println(pq.toString());
+        pq.add(3);
+        System.out.println(pq.toString());
+        pq.iterator();
+        System.out.println(pq.toString());
+        pq.iterator();
+        System.out.println(pq.toString());
+        pq.add(6);
+        System.out.println(pq.toString());
+        pq.remove(6);
+        System.out.println(pq.toString());
 
 
-        binHeap.add(1);
-        binHeap.add(2);
-        binHeap.add(3);
-        binHeap.add(4);
-        binHeap.add(5);
-        binHeap.add(6);
-        binHeap.add(7);
-        binHeap.add(8);
-                        // lalala
-        System.out.println(binHeap.toString());
+
+
     }
 }
