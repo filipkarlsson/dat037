6dcef7d7c1cc8d5d0499e056cc921508b18eb1af
Filip Karlsson <filip@filipkarlsson.com>
2017-12-04 15:30:14 +0100

    Part A with time complexity analysis finished

--- Laboration3/src/Graph.java
+++ Laboration3/src/Graph.java
@@ -1,11 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Graph {
-    public Graph() { }
+    private ArrayList<Pair<String, PrioMap<String,Integer>>> nodes;
 
-    public void addVertice(String label) { return; }
+    public Graph() {
+        nodes = new ArrayList<>();
+    }
 
-    public void addEdge(String node1, String node2, int dist) { return; }
+    public void addVertice(String label) {
+        nodes.add(new Pair<>(label, new APrioMap<>()));
+    }
+
+    public void addEdge(String node1, String node2, int dist) {
+        for (Pair<String, PrioMap<String,Integer>> p : nodes) {
+            if (p.a.equals(node1)){
+                p.b.put(node2,dist);
+            }
+            if (p.a.equals(node2)){
+                p.b.put(node1,dist);
+            }
+        }
+
+    }
 
     public static class Path {
         public int totalDist;
@@ -21,5 +39,8 @@ public class Graph {
         }
     }
 
-    public Path shortestPath(String start, String dest) { return null; }
+    public Path shortestPath(String start, String dest) {
+        HashMap<String,Pair<>>
+
+    }
 }
